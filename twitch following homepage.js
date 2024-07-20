8735ef4690a7205ee727db82bae7729442733e8b
s1c <65496215+s1csty9@users.noreply.github.com>
2024-07-20 03:02:57 -0400

    Create twitch following homepage.js

---
+++ twitch following homepage.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Twitch following homepage
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Redirects the twitch homepage to following page
+// @author       s1csty9
+// @match        *://www.twitch.tv/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (window.location.href !== 'https://www.twitch.tv/directory/following') {
+        window.location.replace('https://www.twitch.tv/directory/following');
+    }
+})();
